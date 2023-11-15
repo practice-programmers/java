4f6485ef48865fdcee94df56b4179d1eafd77301
ryehwa <rhk415@gmail.com>
2023-11-15 09:49:36 +0900

    Add _1115

--- src/main/java/nov/_1114/_1114.java
+++ src/main/java/nov/_1114/_1114.java
@@ -1,2 +1,26 @@
-package nov._1114;public class _1114 {
+package nov._1114;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class _1114 {
+    public int[] solution(int start_num, int end_num) {
+        //list 길이 설정
+        int listLength = end_num - start_num + 1;
+        int[] answer = new int[listLength];
+        //for문으로 리스트에 넣기
+        if(start_num >= 0 && start_num <= end_num && end_num <= 50) {
+            for (int i = 0; i < listLength; i++) {
+                answer[i] = start_num + i;
+            }
+            return answer;
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        _1114 test = new _1114();
+        int[] result = test.solution(3,7);
+        System.out.println(Arrays.toString(result));
+    }
 }
---
+++ src/main/java/nov/_1115/_1115.java
@@ -0,0 +1,30 @@
+package nov._1115;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class _1115 {
+    public int[] solution(int[] arr) {
+        //결과 넣을 새로운 int[] 생성
+        int arrLength = arr.length;
+        int[] answer = new int[arrLength];
+        //for문으로 arr의 값들 추출해서 계산해서 answer 배열에 넣기
+        for(int i = 0; i < arrLength; i++) {
+            if(arr[i] >= 50 && arr[i] % 2 == 0) {
+                answer[i] = arr[i] / 2;
+            } else if (arr[i] < 50 && arr[i] % 2 == 1) {
+                answer[i] = arr[i] * 2;
+            } else {
+                answer[i] = arr[i];
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        _1115 test = new _1115();
+        int[] testArr = {1,50,79};
+        int[] result = test.solution(testArr);
+        System.out.println(Arrays.toString(result));
+    }
+}
