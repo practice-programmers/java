974b74ec177e69af3befab587f3d24cbaf6194f9
ryehwa <rhk415@gmail.com>
2023-11-27 10:07:59 +0900

    Add _1127

---
+++ src/main/java/nov/_1127/_1127.java
@@ -0,0 +1,10 @@
+package nov._1127;
+public class _1127 {
+    public String solution(String my_string, int k) {
+        String answer = "";
+        for(int i = 0; i < k; i++) {
+            answer += my_string;
+        }
+        return answer;
+    }
+}
