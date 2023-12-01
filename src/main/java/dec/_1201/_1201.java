252943a7f70a8eb5e241ac93e5e59aae2a10d477
ryehwa <rhk415@gmail.com>
2023-12-01 09:57:11 +0900

    Add _1201

---
+++ src/main/java/dec/_1201/_1201.java
@@ -0,0 +1,8 @@
+package dec._1201;
+
+public class _1201 {
+    public String solution(String my_string, int n) {
+        String answer = my_string.substring(my_string.length() - n);
+        return answer;
+    }
+}
