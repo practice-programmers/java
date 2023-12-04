240e1213e60a437f31e8bfafef849f5fa0f8325d
ryehwa <rhk415@gmail.com>
2023-12-04 10:14:52 +0900

    Add _1202

---
+++ src/main/java/dec/_1202/_1202.java
@@ -0,0 +1,7 @@
+package dec._1202;
+
+public class _1202 {
+    public String solution(String my_string, int n) {
+        return my_string.substring(0,n);
+    }
+}
---
+++ src/main/java/dec/_1203/_1203.java
@@ -0,0 +1,2 @@
+package dec._1203;public class _1203 {
+}
