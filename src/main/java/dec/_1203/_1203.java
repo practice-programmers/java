0e3b0a0a2570e2be2e162a79bcce207ead41a49d
ryehwa <rhk415@gmail.com>
2023-12-04 10:15:43 +0900

    Add 1203

--- src/main/java/dec/_1203/_1203.java
+++ src/main/java/dec/_1203/_1203.java
@@ -1,2 +1,30 @@
-package dec._1203;public class _1203 {
+package dec._1203;
+
+public class _1203 {
+    public int solution(int n, String control) {
+        int result = n;
+        String[] arr = control.split("");
+        for (String index : arr) {
+            switch (index) {
+                case "w":
+                    result += 1;
+                    break;
+                case "s":
+                    result -= 1;
+                    break;
+                case "d":
+                    result += 10;
+                    break;
+                case "a":
+                    result -= 10;
+                    break;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        _1203 test = new _1203();
+        System.out.println(test.solution(0, "wsdawsdassw"));
+    }
 }
