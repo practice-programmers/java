5af0930e7df1d27c866b9fb25fee7e5abb82ab55
ryehwa <rhk415@gmail.com>
2023-12-07 10:59:16 +0900

    Add _1207

---
+++ src/main/java/dec/_1207/_1207.java
@@ -0,0 +1,10 @@
+package dec._1207;
+
+import java.util.Arrays;
+
+public class _1207 {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = Arrays.copyOfRange(num_list, 0, n);
+        return answer;
+    }
+}
