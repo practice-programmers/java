57303a894c5a33a7555789bf57230639f16ddece
ryehwa <rhk415@gmail.com>
2023-12-28 23:11:54 +0900

    Add _1210

--- src/main/java/dec/_1210/_1210.java
+++ src/main/java/dec/_1210/_1210.java
@@ -1,4 +1,12 @@
 package dec._1210;
 
 public class _1210 {
+    public int[] solution(int n, int k) {
+        int answer_length = (int) Math.floor(n / k);
+        int[] answer = new int[answer_length];
+        for (int i = 0; i < answer_length; i++) {
+            answer[i] = k * (i + 1);
+        }
+        return answer;
+    }
 }
