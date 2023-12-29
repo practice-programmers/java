0f322ab289d8ec987b88d2c5fa79c07b3936f321
ryehwa <rhk415@gmail.com>
2023-12-29 21:34:06 +0900

    Add _1213

---
+++ src/main/java/dec/_1213/_1213.java
@@ -0,0 +1,12 @@
+package dec._1213;
+
+public class _1213 {
+    public int[] solution(int start, int end_num) {
+        int answerLength = start - end_num + 1;
+        int[] answer = new int[answerLength];
+        for(int i = 0; i < answerLength; i++) {
+            answer[i] = start - i;
+        }
+        return answer;
+    }
+}
