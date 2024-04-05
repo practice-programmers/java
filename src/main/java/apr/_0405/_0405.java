394d91dfe0c19b2a535c5fe16efc9eb031e39226
ryehwa <rhk415@gmail.com>
2024-04-05 16:35:27 +0900

    Add _0405

---
+++ src/main/java/apr/_0405/_0405.java
@@ -0,0 +1,10 @@
+package apr._0405;
+
+import java.util.Locale;
+
+public class _0405 {
+    public int solution(String myString, String pat) {
+    if((myString.toUpperCase()).contains(pat.toUpperCase())) return 1;
+    return 0;
+}
+}
